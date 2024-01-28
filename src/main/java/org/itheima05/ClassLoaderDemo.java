c695a101a23711f837d3d9d7fde6de6ee479017b
white-gardenia613 <2571358956@qq.com>
2024-01-28 14:51:03 +0800

    类加载器

---
+++ src/main/java/org/itheima05/ClassLoaderDemo.java
@@ -0,0 +1,14 @@
+package org.itheima05;
+
+public class ClassLoaderDemo {
+    public static void main(String[] args) {
+        ClassLoader c=ClassLoader.getSystemClassLoader();
+        System.out.println(c);
+
+        ClassLoader c2=c.getParent();
+        System.out.println(c2);
+
+        ClassLoader c3=c2.getParent();
+        System.out.println(c3);
+    }
+}
